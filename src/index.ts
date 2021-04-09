8df28b7642562f2128a7813bcea510e9a4635594
Henrique Curzio Ladeira <henrique.c.ladeira@gmail.com>
2021-04-08 22:32:43 -0300

    test

--- src/index.ts
+++ src/index.ts
@@ -10,5 +10,5 @@ void (async (): Promise<void> => {
   // create db connection
   await createConnection();
   // start express server
-  app.listen(process.env.PORT, () => console.log('Oh GEE I\'m up on PORT'));
+  app.listen(process.env.PORT ?? 3000, () => console.log('Oh GEE I\'m up on PORT'));
 })();
