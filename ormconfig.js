97da37842b17cb52d11d93cacbd5391515070494
Henrique Curzio Ladeira <henrique.c.ladeira@gmail.com>
2021-04-11 13:12:02 -0300

    fix local settings

--- ormconfig.js
+++ ormconfig.js
@@ -2,9 +2,46 @@ const PostgressConnectionStringParser = require('pg-connection-string');
 
 // const database = (process.env.mode === 'TEST') ? process.env.DB_NAME_TEST : process.env.DB_NAME;
 const dropSchema = (process.env.mode === 'TEST');
+const ssl = (process.env.NODE_ENV === 'production') ? { rejectUnauthorized: false } : false;
 
 const databaseUrl = process.env.DATABASE_URL;
-const connectionOptions = PostgressConnectionStringParser.parse(databaseUrl);
+const connectionOptions = (process.env.mode === 'TEST')
+  ? PostgressConnectionStringParser.parse(`${databaseUrl}_test`)
+  : PostgressConnectionStringParser.parse(databaseUrl);
+
+const config = (process.env.NODE_ENV === 'production')
+  ? {
+      entities: [
+        'dist/entity/**/*.js'
+      ],
+      migrations: [
+        'dist/migration/**/*.js'
+      ],
+      subscribers: [
+        'dist/subscriber/**/*.js'
+      ],
+      cli: {
+        entitiesDir: 'dist/entity',
+        migrationsDir: 'dist/migration',
+        subscribersDir: 'dist/subscriber'
+      }
+    }
+  : {
+      entities: [
+        'src/entity/**/*.ts'
+      ],
+      migrations: [
+        'src/migration/**/*.ts'
+      ],
+      subscribers: [
+        'src/subscriber/**/*.ts'
+      ],
+      cli: {
+        entitiesDir: 'src/entity',
+        migrationsDir: 'src/migration',
+        subscribersDir: 'src/subscriber'
+      }
+    };
 
 const ormconfig = {
   type: 'postgres',
@@ -15,22 +52,9 @@ const ormconfig = {
   password: connectionOptions.password,
   dropSchema,
   synchronize: true,
-  ssl: { rejectUnauthorized: false },
+  ssl,
   logging: false,
-  entities: [
-    'dist/entity/**/*.js'
-  ],
-  migrations: [
-    'dist/migration/**/*.js'
-  ],
-  subscribers: [
-    'dist/subscriber/**/*.js'
-  ],
-  cli: {
-    entitiesDir: 'dist/entity',
-    migrationsDir: 'dist/migration',
-    subscribersDir: 'dist/subscriber'
-  }
+  ...config
 };
 
 module.exports = ormconfig;
