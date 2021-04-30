5d24641508365741f53a9a081789632360ab834d
Raul Max <raulmax319@gmail.com>
2021-04-29 22:04:07 -0300

    ✨feat: user repository & remove user method

--- src/repositories/user-repository.ts
+++ src/repositories/user-repository.ts
@@ -1,16 +1,41 @@
-import { getRepository } from 'typeorm';
-import { User } from '~/entity';
+import { getRepository, Repository } from "typeorm";
+import { User } from "~/entity";
 
-export class UserRepository {
-  private readonly userRepository = getRepository(User)
+export class UserRepository extends Repository<User> {
+  private readonly userRepository = getRepository(User);
 
   public getUsers = async (): Promise<User[]> => {
     const users = await this.userRepository.find();
     return users;
-  }
+  };
 
   public getOne = async (userId: string): Promise<newUser> => {
     const user = await this.userRepository.findOne(userId);
     return user;
-  }
+  };
+
+  public saveUser = async (newUser: newUser): Promise<safeUser> => {
+    try {
+      const user = await this.userRepository.save(newUser);
+      const safeUser: safeUser = user;
+
+      return safeUser;
+    } catch {
+      throw new Error("could not save to database");
+    }
+  };
+
+  public removeUser = async (userId: string): Promise<void> => {
+    try {
+      const user = await this.userRepository.findOne(userId);
+      try {
+        await this.userRepository.remove(user);
+        console.log("Successfully deleted.");
+      } catch {
+        throw new Error("Something went wrong with out internal servers.");
+      }
+    } catch {
+      throw new Error("User not found.");
+    }
+  };
 }
