f57d4ce045987b693b46cd9e9a387a5b5682b7db
Raul Max <raulmax319@gmail.com>
2021-04-29 22:03:06 -0300

    ⚙️refactor: method names & dependency injection

--- src/model/user.ts
+++ src/model/user.ts
@@ -1,38 +1,44 @@
 import { UserRepository } from "~/repositories/user-repository";
 
 export class UserModel {
-  constructor (private readonly userRepository: UserRepository) {}
+  constructor(private readonly userRepository: UserRepository) {}
 
-  public getAll = async (): Promise<safeUser[]> => {
+  public findAll = async (): Promise<safeUser[]> => {
     const users = await this.userRepository.getUsers();
     const safeUsers = users.map((elem) => {
       const { password, id, ...safeUser } = elem;
       return safeUser;
     });
     return safeUsers;
-  }
+  };
 
-  public getOne = async (userId: string): Promise<safeUser> => {
-    if (!userId) throw new Error('Please provide an ID to search');
+  public findOne = async (userId: string): Promise<safeUser> => {
+    if (!userId) throw new Error("Please provide an ID to search");
 
-    const user = this.userRepository.getOne(userId);
-    if (!user) throw new Error('There is no such user.');
+    const user: Promise<safeUser> = this.userRepository.getOne(userId);
+    if (!user) throw new Error("There is no such user.");
 
-    const { password, ...safeUser } = user;
-    return safeUser;
-  }
+    return user;
+  };
 
-  public save = async (newUser: newUser): Promise<safeUser> => {
+  public addUser = async (newUser: newUser): Promise<safeUser> => {
+    if (!newUser) throw new Error("Missing fields");
+
     try {
-      await this.userRepository.save(newUser);
-      const { password, ...safeUser } = newUser;
-      return safeUser;
+      const user = await this.userRepository.saveUser(newUser);
+      return user;
     } catch {
-      throw new Error('could not save to database');
+      throw new Error("Failed to save.");
     }
-  }
+  };
 
-  public remove = async (userId): Promise<void> => {
-    throw new Error('not implemented yet');
-  }
+  public removeUser = async (userId: string): Promise<void> => {
+    if (!userId) throw new Error("You must provide the user's ID to remove");
+
+    try {
+      await this.userRepository.removeUser(userId);
+    } catch {
+      throw new Error("Something went wrong with the request.");
+    }
+  };
 }
